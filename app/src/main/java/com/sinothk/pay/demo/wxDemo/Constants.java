60f34aad73227538c81279bcb500d300f5388082
LiangYT <381518188@qq.com>
2019-12-30 16:44:56 +0800

    初始化！

--- app/src/main/java/com/sinothk/pay/demo/wxDemo/Constants.java
+++ app/src/main/java/com/sinothk/pay/demo/wxDemo/Constants.java
@@ -2,8 +2,8 @@ package com.sinothk.pay.demo.wxDemo;
 
 public class Constants {
 
-	// APP_ID 替换为你的应用从官方网站申请到的合法appId
-    public static final String APP_ID = "wxe11f60fc197fc8a5";
+	// APP_ID 替换为你的应用从官方网站申请到的合法appId wxe11f60fc197fc8a5
+    public static final String APP_ID = "wxb4ba3c02aa476ea1";
 
     public static class ShowMsgActivity {
 		public static final String STitle = "showmsg_title";
